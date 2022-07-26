40d95d446c8c65aeb8bc7a1138e6a5b05a020921
Chris Jang <62946867+itsChrisJang@users.noreply.github.com>
2022-07-26 18:24:33 +0900

    release: add project file to study Connection JDBC
    강의 :
    스프링 DB 1편 - 데이터 접근 핵심 원리
    
    -. add project file to study Connection, PreparedStatement

---
+++ study-spring-db1/jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -0,0 +1,35 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class MemberRepositoryVOTest {
+
+    MemberRepositoryVO repository = new MemberRepositoryVO();
+
+    @Test
+    void save() throws SQLException {
+        // save
+        Member member = new Member("memberV6", 14000);
+        repository.save(member);
+
+        // findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        // update : money: 10,000 -> 20,000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+    }
+}
