dcb7ee5c366609f7475d9380683d8a6269bccba8
itsChrisJang <qiiopasd@naver.com>
2022-07-28 15:12:43 +0900

    feat: add files to study db transaction

---
+++ study-spring-db1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,31 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepository;
+
+    // 트랜잭션을 사용하는 동안 같은 커넥션을 유지해야함.
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 시작
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+        // 커밋, 롤벡
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
