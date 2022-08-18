55c763f551dd38d1ec505e811bb0469f68d85626
itsChrisJang <qiiopasd@naver.com>
2022-08-18 17:18:06 +0900

    feat: add file about entityContext
    -. add file about entityContext

--- study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,6 +20,8 @@ public class JpaMain {
         try {
 //            Member findMember = entityManager.find(Member.class, 1L);
 
+
+            /*
             List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(5)
                     .setMaxResults(8)
@@ -28,6 +30,18 @@ public class JpaMain {
             for (Member member : result) {
                 System.out.println("member.getName() = " + member.getName());
             }
+            */
+
+            // 영속성 컨텍스트
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            entityManager.persist(member);
+            System.out.println("=== AFTER ===");
 
             tx.commit();
         } catch (Exception e) {
