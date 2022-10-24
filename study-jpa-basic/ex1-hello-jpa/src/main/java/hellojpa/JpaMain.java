056ba1baec69c21f7c1661f17fba8cbe763ba8e7
itsChrisJang <qiiopasd@naver.com>
2022-10-24 22:04:37 +0900

    feat: study JPA proxy
    :

--- study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,12 @@
 package hellojpa;
 
+import org.hibernate.Hibernate;
+
+import javax.lang.model.SourceVersion;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -18,15 +20,22 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("chris1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
             em.flush();
             em.clear();
+
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass());
+            // 프록시 강제 초기화
+            // 1.
+            refMember.getUsername();
+            // 2.
+//            Hibernate.initialize(refMember);
+
+//            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
 
             tx.commit();
         } catch (Exception e) {
@@ -38,4 +47,17 @@ public class JpaMain {
 
         emf.close();
     }
+
+    private static void printMember(Member member) {
+        System.out.println("member = " + member.getUsername());
+    }
+
+    private static void printMemberAndTeam(Member member) {
+        String username = member.getUsername();
+        System.out.println("username = " + username);
+
+        Team team = member.getTeam();
+        System.out.println("team.getName() = " + team.getName());
+    }
 }
+
--- study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,8 +1,6 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Member extends BaseEntity {
@@ -19,13 +17,6 @@ public class Member extends BaseEntity {
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts = new ArrayList<>();
-
     public Long getId() {
         return id;
     }
@@ -40,5 +31,13 @@ public class Member extends BaseEntity {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
