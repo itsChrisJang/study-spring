3676bcd0d187776e6c90cdd44f4243191af5dd86
itsChrisJang <qiiopasd@naver.com>
2022-10-27 23:58:42 +0900

    feat: study JPA CASCADE

--- study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,31 +21,16 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team teamB = new Team();
-            teamB.setName("teamA");
-            em.persist(teamB);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(teamB);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-
-//            Member m = em.find(Member.class, member1.getId());
-
-            List<Member> members = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
+            em.persist(parent);
+            em.persist(child1);
+            em.persist(child2);
 
             tx.commit();
         } catch (Exception e) {
