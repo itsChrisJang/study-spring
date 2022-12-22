a7108545e98854634938e25a8f97b8e0299c9e51
itsChrisJang <qiiopasd@naver.com>
2022-12-22 23:59:45 +0900

    feat:study identity and equivalence

--- study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ study-jpa-basic/ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -1,14 +1,20 @@
 package hellojpa;
 
+import javax.lang.model.SourceVersion;
+
 public class ValueMain {
 
     public static void main(String[] args) {
 
-        Integer a = new Integer(10);
-        Integer b = a;
+        int a = 10;
+        int b = 10;
 
-        a = new Integer(20);
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("a == b : " + (a == b));
+
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+
+        System.out.println("address1 == address2 : " + (address1 == address2));
+        System.out.println("address1.equals(address2) = " + address1.equals(address2));
     }
 }
