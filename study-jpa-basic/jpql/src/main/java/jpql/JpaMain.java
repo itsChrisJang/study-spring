c206df26f2ac319d549a0f0573bb8bf6f399ff6a
itsChrisJang <qiiopasd@naver.com>
2022-12-27 17:48:08 +0900

    feat: study JPQL subquery and case

--- study-jpa-basic/jpql/src/main/java/jpql/JpaMain.java
+++ study-jpa-basic/jpql/src/main/java/jpql/JpaMain.java
@@ -28,6 +28,7 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             member.setAge(20);
+            member.setType(MemberType.ADMIN);
 
             member.setTeam(team);
 
@@ -36,15 +37,17 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m inner join m.team t";
-            List<Member> result = em.createQuery(query, Member.class)
+            String query = "select m.username, 'HELLO', TRUE from Member m " +
+                        "where m.type = :userType";
+            List<Object[]> result = em.createQuery(query)
+                    .setParameter("userType", MemberType.ADMIN)
                     .getResultList();
 
-            System.out.println("result.size() = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
+            for (Object[] objects : result) {
+                System.out.println("objects = " + objects[0]);
+                System.out.println("objects = " + objects[1]);
+                System.out.println("objects = " + objects[2]);
             }
-
 
             tx.commit();
         } catch (Exception e) {
--- study-jpa-basic/jpql/src/main/java/jpql/Member.java
+++ study-jpa-basic/jpql/src/main/java/jpql/Member.java
@@ -14,6 +14,8 @@ public class Member {
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
+    private MemberType type;
+
     public void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
@@ -49,6 +51,14 @@ public class Member {
 
     public void setTeam(Team team) {
         this.team = team;
+    }
+
+    public MemberType getType() {
+        return type;
+    }
+
+    public void setType(MemberType type) {
+        this.type = type;
     }
 
     @Override
