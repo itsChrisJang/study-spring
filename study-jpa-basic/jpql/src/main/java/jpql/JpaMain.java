54dfe5ef9272d16f0b729b3599f4d64cce6f4cfb
itsChrisJang <qiiopasd@naver.com>
2022-12-28 01:44:19 +0900

    feat: study JPA Path Expression

--- study-jpa-basic/jpql/src/main/java/jpql/JpaMain.java
+++ study-jpa-basic/jpql/src/main/java/jpql/JpaMain.java
@@ -1,12 +1,10 @@
 package jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -26,28 +24,27 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername("관리자1");
             member.setAge(20);
-            member.setType(MemberType.ADMIN);
-
             member.setTeam(team);
-
+            member.setType(MemberType.ADMIN);
             em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자1");
+            member2.setAge(20);
+            member2.setTeam(team);
+            member2.setType(MemberType.ADMIN);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select m.username, 'HELLO', TRUE from Member m " +
-                        "where m.type = :userType";
-            List<Object[]> result = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
-                    .getResultList();
+            String query = "select m.username from Team t join Member m";
 
-            for (Object[] objects : result) {
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
-            }
+            List<Collection> result = em.createQuery(query, Collection.class).getResultList();
+
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
