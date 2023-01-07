f12f7fabe2d07231210b54a8f92119a2a919e6d3
itsChrisJang <qiiopasd@naver.com>
2023-01-07 23:15:45 +0900

    feat: add entity class files for example

--- study-jpa-practice1/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ study-jpa-practice1/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,26 +1,24 @@
 package jpabook.jpashop.domain;
 
-
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter @Setter
-public class Member {
+public class Delivery {
 
     @Id @GeneratedValue
-    @Column(name = "member_id")
+    @Column(name = "delivery_id")
     private Long id;
 
-    private String name;
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
 
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = "member")
-    private List<Order> orders = new ArrayList<>();
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // READE, COMP
 }
