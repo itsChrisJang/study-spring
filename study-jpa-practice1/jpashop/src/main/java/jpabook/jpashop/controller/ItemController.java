85ac04a230d96243dbd9fb01104cf3f07dfebe6e
itsChrisJang <qiiopasd@naver.com>
2023-01-26 18:25:23 +0900

    feat: add files about product order

--- study-jpa-practice1/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ study-jpa-practice1/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -64,20 +64,23 @@ public class ItemController {
     }
 
     @PostMapping("items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form, @PathVariable String itemId){
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form){
 
+        // 주의사항
         // 1. 이 user가 해당 item에 권한이 있는지 check 해야 한다.
         // 2. update할 객체를 session에 담아서 푼다.
 
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//        itemService.saveItem(book);
 
-        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
+
         return "redirect:/items";
     }
 }
--- study-jpa-practice1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ study-jpa-practice1/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -22,11 +22,11 @@ public class ItemService {
     }
 
     @Transactional
-    public void updateItem(Long itemId, Book param) {
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
         Item findItem = itemRepository.findOne(itemId);
-        findItem.setPrice(param.getPrice());
-        findItem.setName(param.getName());
-        findItem.setStockQuantity(param.getStockQuantity());
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
