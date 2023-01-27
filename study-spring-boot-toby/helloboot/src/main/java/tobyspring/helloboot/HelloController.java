940fca0b1a2765d223d383cc346ad7d498487299
itsChrisJang <qiiopasd@naver.com>
2023-01-27 23:14:55 +0900

    refactor: modify Application's start with DispactherServlet

--- study-spring-boot-toby/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ study-spring-boot-toby/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,13 +1,23 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.util.Objects;
+
+@RequestMapping
 public class HelloController {
 
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
     @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
-        return "Hello " + name;
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
--- study-spring-boot-toby/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ study-spring-boot-toby/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,40 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.registerBean(SimpleHelloService.class);
+		applicationContext.refresh();
+
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("dispatcherServlet",
+						new DispatcherServlet(applicationContext)
+					).addMapping("/*");
+		});
+		webServer.start();
 	}
 
 }
+
