a4f5647dbf5118a796c5c86a2f61f376fcba6e4c
itsChrisJang <qiiopasd@naver.com>
2023-01-29 22:14:50 +0900

    refactor: add MySpringApplcation class to replace SpringApplication class

--- study-spring-boot-toby/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ study-spring-boot-toby/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,21 +1,27 @@
 package tobyspring.helloboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class HelloController {
+public class HelloController  {
 
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
 }
--- study-spring-boot-toby/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ study-spring-boot-toby/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,9 +1,11 @@
 package tobyspring.helloboot;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -13,24 +15,22 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory servletWebServerFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
+//	public static void main(String[] args) {
+//		MySpringApplication.run(HellobootApplication.class, args);
+//	}
 
 	public static void main(String[] args) {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-							new DispatcherServlet(this)
-					).addMapping("/*");
-				});
-				webServer.start();
-			}
-		};
-		applicationContext.register(HellobootApplication.class);
-		applicationContext.refresh();
+		SpringApplication.run(HellobootApplication.class, args);
 	}
 }
 
