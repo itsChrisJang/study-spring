5572089672fe97f3446136cc68df05a437cfb5b0
itsChrisJang <qiiopasd@naver.com>
2023-02-01 20:30:03 +0900

    feat: study Configuration's proxyBeanMethods option

---
+++ study-spring-boot-toby/helloboot/src/test/java/tobyspring/study/ConfigurationTest.java
@@ -0,0 +1,101 @@
+package tobyspring.study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ConfigurationTest {
+    @Test
+    public void configruration() throws Exception {
+        //given
+        MyConfig myConfig = new MyConfig();
+        Bean1 bean1 = myConfig.bean1();
+        Bean2 bean2 = myConfig.bean2();
+
+        //when
+        
+        //then
+        Assertions.assertThat(bean1.common).isNotSameAs(bean2.common);
+    }
+
+    @Test
+    public void configruration2() throws Exception {
+        //given
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+        ac.register(MyConfig.class);
+        ac.refresh();
+
+        Bean1 bean1 = ac.getBean(Bean1.class);
+        Bean2 bean2= ac.getBean(Bean2.class);
+
+        //when
+
+        //then
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    @Test
+    void proxyCommonMethod() {
+        MyConfigProxy myConfigProxy = new MyConfigProxy();
+
+        Bean1 bean1 = myConfigProxy.bean1();
+        Bean2 bean2 = myConfigProxy.bean2();
+
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    static class MyConfigProxy extends MyConfig {
+        private Common common;
+
+        @Override
+        Common common() {
+            if(this.common == null) {
+                this.common = super.common();
+            }
+
+            return this.common;
+        }
+    }
+
+    @Configuration(proxyBeanMethods = true)
+    static class MyConfig {
+        @Bean
+        Common common() {
+            return new Common();
+        }
+
+        @Bean
+        Bean1 bean1() {
+            return new Bean1(common());
+        }
+
+        @Bean
+        Bean2 bean2() {
+            return new Bean2(common());
+        }
+    }
+
+    static class Bean1 {
+        private final Common common;
+
+        Bean1(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Bean2 {
+        private final Common common;
+
+        Bean2(Common common) {
+            this.common = common;
+        }
+    }
+
+    static class Common {
+    }
+
+    // Bean1 <-- Common
+    // Bean2 <-- Common
+}
