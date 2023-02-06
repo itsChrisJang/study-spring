b819f933b9bbf5b3374b5687271b7fb40d255d56
itsChrisJang <qiiopasd@naver.com>
2023-02-06 23:57:51 +0900

    feat: add HelloJobConfiguration

---
+++ study-spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/job/HelloJobConfiguration.java
@@ -0,0 +1,37 @@
+package io.springbatch.springbatchlecture.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public HelloJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("hellloob")
+                .start(helloStep())
+                .build();
+    }
+
+    @Bean
+    public Step helloStep() {
+        return stepBuilderFactory.get("helloStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Hello Spring Batch");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
