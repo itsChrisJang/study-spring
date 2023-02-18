2b28c8d45abc46b4bdb871d8135af83ef6a7a525
itschrisjang <qiiopasd@naver.com>
2023-02-18 15:53:55 +0900

    Feat: Study ExitStatus and JobExecutionDecider

--- study-spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/job/config/sector/flowjob/transition/CustomExitStatusConfiguration.java
+++ study-spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/job/config/sector/flowjob/transition/CustomExitStatusConfiguration.java
@@ -1,7 +1,9 @@
 /*
 package io.springbatch.springbatchlecture.job.config.sector.flowjob.transition;
 
+import io.springbatch.springbatchlecture.job.config.sector.flowjob.transition.listener.PassCheckingListener;
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -12,27 +14,20 @@ import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class TransitionConfiguration {
+public class CustomExitStatusConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    // Transition - on() / to() / stop(), fail(), end(), stopAndRestart()
+    // 사용자 정의 ExitStatus
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get("batchJob")
                 .start(step1())
                     .on("FAILED")
                     .to(step2())
-                    .on("FAILED")
+                    .on("PASS")
                     .stop()
-                .from(step1())
-                    .on("*")
-                    .to(step3())
-                    .next(step4())
-                .from(step2())
-                    .on("*")
-                    .to(step5())
                 .end()
                 .build();
     }
@@ -42,7 +37,7 @@ public class TransitionConfiguration {
         return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println(">>>> step1 has executed ");
-//                    contribution.setExitStatus(ExitStatus.FAILED);
+                    contribution.getStepExecution().setExitStatus(ExitStatus.FAILED);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
@@ -53,40 +48,11 @@ public class TransitionConfiguration {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println(">>>> step2 has executed ");
-//                    contribution.setExitStatus(ExitStatus.FAILED);
                     return RepeatStatus.FINISHED;
                 })
+                .listener(new PassCheckingListener())
                 .build();
     }
 
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">>>> step3 has executed ");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">>>> step4 has executed ");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println(">>>> step5 has executed ");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
 }
 */
