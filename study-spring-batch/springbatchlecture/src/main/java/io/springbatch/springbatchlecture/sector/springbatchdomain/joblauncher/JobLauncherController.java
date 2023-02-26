d6ef23066f648b1c901006f0a5e26fab17b18ba2
itsChrisJang <qiiopasd@naver.com>
2023-02-27 00:05:42 +0900

    Feat: Study JobLauncher with asyncTakeExecutor

--- study-spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/sector/springbatchdomain/joblauncher/JobLauncherController.java
+++ study-spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/sector/springbatchdomain/joblauncher/JobLauncherController.java
@@ -5,10 +5,13 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.batch.BasicBatchConfigurer;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +23,8 @@ import java.util.Date;
 public class JobLauncherController {
 
     @Autowired private Job job;
-    @Autowired private JobLauncher jobLauncher;
+//    @Autowired private JobLauncher jobLauncher;
+    @Autowired private BasicBatchConfigurer basicBatchConfigurer;
 
     @PostMapping("/batch")
     public String launch(@RequestBody Member member) throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
@@ -29,6 +33,8 @@ public class JobLauncherController {
                 .addDate("date", new Date())
                 .toJobParameters();
 
+        SimpleJobLauncher jobLauncher = (SimpleJobLauncher) basicBatchConfigurer.getJobLauncher();
+        jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
         jobLauncher.run(job, jobParameters);
 
         return "batch completed";
