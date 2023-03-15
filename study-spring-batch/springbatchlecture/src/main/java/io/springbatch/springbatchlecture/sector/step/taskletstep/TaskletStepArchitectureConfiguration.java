90a0262ae998caa8125d3d1830e17d03700c7864
itsChrisJang <qiiopasd@naver.com>
2023-03-15 23:42:13 +0900

    Feat: Study TaskletStep Architecture

--- study-spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/sector/step/taskletstep/TaskletStepArchitectureConfiguration.java
+++ study-spring-batch/springbatchlecture/src/main/java/io/springbatch/springbatchlecture/sector/step/taskletstep/TaskletStepArchitectureConfiguration.java
@@ -1,10 +1,7 @@
-/*
 package io.springbatch.springbatchlecture.sector.step.taskletstep;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -16,31 +13,31 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class StartLimitAndAllowStartIfCompleteConfiguration {
+public class TaskletStepArchitectureConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    */
-/*
-        startLimit()
-            - Step의 실행 횟수를 조정할 수 있다.
-            - Step 마다 설정할 수 있다.
-            - 설정 값을 초과해서 다시 실행하려고 하면 StartLimitExceededException이 발생
-            - start-limit의 디폴트 값은 Integer.MAX_VALUE
-        allowStartIfComplete()
-            - 재시작 가능한 job 에서 Step 의 이전 성공 여부와 상관없이 항상 step 을 실행하기 위한 설정
-            - 실행 마다 유효성을 검증하는 Step이나 사전 작업이 꼭 필요한 Step 등
-            - 기본적으로 COMPLETED 상태를 가진 Step 은 Job 재 시작시 실행하지 않고 스킵한다.
-            - allow-start-if-complete가 "true"로 설정된 stop은 항상 실행한다.
-     *//*
-
+    /*
+        TaskletStep Architecture
+     */
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get("batchJob")
                 .incrementer(new RunIdIncrementer())
                 .start(step1())
                 .next(step2())
+                .listener(new StepExecutionListener() {
+                    @Override
+                    public void beforeStep(StepExecution stepExecution) {
+
+                    }
+
+                    @Override
+                    public ExitStatus afterStep(StepExecution stepExecution) {
+                        return null;
+                    }
+                })
                 .build();
     }
 
@@ -54,7 +51,6 @@ public class StartLimitAndAllowStartIfCompleteConfiguration {
                         return RepeatStatus.FINISHED;
                     }
                 })
-                .allowStartIfComplete(true)
                 .build();
     }
 
@@ -65,13 +61,10 @@ public class StartLimitAndAllowStartIfCompleteConfiguration {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("StepContribution = " + stepContribution + ", ChunkContext = " + chunkContext);
-                        throw new RuntimeException("step2 was failed");
-//                        return RepeatStatus.FINISHED;
+                        return RepeatStatus.FINISHED;
                     }
                 })
-                .startLimit(3)
                 .build();
     }
 
 }
-*/
