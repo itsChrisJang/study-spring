a7d45b6dd8690ae6add24d24bd9684042a520ca3
itsChrisJang <qiiopasd@naver.com>
2023-04-06 21:54:32 +0900

    Feat: Add example about range, filter, map

--- study-reactive-rxjava/rxjavachris/src/main/java/com/itvillage/rxjavachris/example/reactiveprogrammingdeeply/assignment_01.java
+++ study-reactive-rxjava/rxjavachris/src/main/java/com/itvillage/rxjavachris/example/reactiveprogrammingdeeply/assignment_01.java
@@ -1,5 +1,13 @@
-package src.main.java.com.itvillage.rxjavachris.example.reactiveprogrammingdeeply;
+package com.itvillage.rxjavachris.example.reactiveprogrammingdeeply;
+
+import io.reactivex.Observable;
 
 public class assignment_01 {
     // 1. range, filter, map을 이용하여 1부터 15 까지의 숫자 중에서 2의 배수만 필터링 한 후, 필터링 된 숫자에 제곱한 숫자를 출력하세요.
+    public static void main(String[] args) {
+        Observable.range(1, 15)
+                .filter(number -> number % 2 == 0)
+                .map(filteredNumber -> filteredNumber * filteredNumber)
+                .subscribe(System.out::println);
+    }
 }
